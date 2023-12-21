c89e12dd74f70f324f8fad4afd5ff2c5eab9c2a0
Fawwazalf <muhammadnafiisfawwazalfarisi@gmail.com>
2023-12-21 09:26:52 +0700

    funfact lang done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,11 +6,10 @@ import MapIndonesia from "@/components/MapIndonesia";
 
 import TitleMap from "@/components/TitleMap";
 import TitleFunFact from "@/components/TitleFunFact";
-import TitleVideo from "@/components/TitleVideo";
-import Image from "next/image";
 
 import dynamic from "next/dynamic";
 import LandingQuiz from "@/components/LandingQuiz";
+import FunFact3 from "@/components/FunFact3";
 
 const Video = dynamic(() => import("@/components/Video"), {
   ssr: false,
@@ -31,19 +30,7 @@ export default function Home() {
         <TitleFunFact />
 
         <div className="flex flex-wrap justify-center gap-6 lg:justify-between w-[80%] md:w-[600px] lg:w-[900px] xl:w-[1050px]">
-          <CardFunfact
-            title="1728 WBTb"
-            desc="Kementerian Pendidikan, Kebudayaan, Riset, dan Teknologi telah menetapkan sebanyak 1728 Warisan Budaya Takbenda (WBTb) Indonesia sejak tahun 2013 hingga 2022
-"
-          />
-          <CardFunfact
-            title="11.622 Budaya"
-            desc="Per November 2022, terdapat 11.622 warisan budaya yang dicatat dan 1.728 di antaranya telah ditetapkan."
-          />
-          <CardFunfact
-            title="peringkat ke-39 sedunia"
-            desc="Kekayaan budaya Indonesia mengantarkan negara yang dilewati khatulistiwa menempati peringkat ke-39 sedunia dalam 'World Cultural Heritage' versi 'World Economic Forum' (WEF)."
-          />
+          <FunFact3 />
         </div>
       </div>
       <LandingQuiz />
